dff3d6c47afa87c89c0e4ccfb6c6eaadc7c4d8d1
Sebastian Krueger <github@kruegermangelsen.com>
2014-01-18 21:25:08 +0100

    added font files for the project

---
+++ window_items.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+graph_objects.py
+
+Created by Sebastian Krüger on 2014-01-18.
+Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+"""
+
+
+class window_items(object):
+
+    def __init__(self, posX, posY, type='Unknown'):
+        self.posX = posX
+        self.posY = posY
+        self.type = type
+        print 'Item.construct: type: ',type
+
+    def getType(self):
+        return self.type
+
+    def move(self,x,y):
+        self.posX = x
+        self.posY = y
+        print 'item.move: move to x=', x, ' y=', y
+
+    def getPos(self):
+        return [self.posX, self.posY]
+
+    def __str__(self):
+        if self.type == 'Text':
+            return "Type: %s, pos: %d:%d" % (self.type, self.posX, self.posY)
+        else:
+            return "Type: unknown"
+
+
+
+class graph_itemTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
